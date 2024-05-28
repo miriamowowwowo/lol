aa9973a2f8794831d0e6b70e49afae55aa8ad9e4
miriamowowwowo <miriamschulk@gmail.com>
2024-05-28 11:02:19 -0300

    terminado ejercicio

---
+++ Assets/LognManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LognManager : MonoBehaviour
+{
+    public string contra;
+    public InputField imputContra;
+    // Start is called before the first frame update
+
+    public void fuuncionPassword()
+    {
+        if (imputContra.text == contra)
+        {
+            Debug.Log("Access granted");
+        }
+        else
+        {
+            Debug.Log("Access denied");
+        }
+    }
+
+}
